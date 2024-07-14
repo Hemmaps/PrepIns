0044a550f8101fbb1a063157a1adfa8598f45fdf
Hemmaps <hemmapssivagnanam@gmail.com>
2024-07-14 20:07:22 +0530

    adding

---
+++ Week-03/Task1.js
@@ -0,0 +1,8 @@
+function tellFortune(numChildren, partnerName, location, jobTitle) {
+    console.log(`You will be a ${jobTitle} in ${location}, and married to ${partnerName} with ${numChildren} kids.`);
+}
+
+
+tellFortune(2, "Emma", "New York", "Software Engineer");
+tellFortune(3, "Michael", "Paris", "Graphic Designer");
+tellFortune(1, "Sophia", "London", "Teacher");
---
+++ Week-03/Task2.js
@@ -0,0 +1,10 @@
+
+
+function calculatePetAge(pet_age,con_rate){
+    pet_age = pet_age * con_rate;
+    console.log(`Your kitty is ${pet_age} years old in cat years!`);
+}
+
+calculatePetAge(4,7);
+calculatePetAge(16,4);
+calculatePetAge(23,9);
